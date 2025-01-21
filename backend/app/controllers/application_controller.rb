b618f065c49f17d6a9e1271df87e9d0e45f5f296
Shigeo Kitamura <shigeo9462@gmail.com>
2025-01-21 14:19:40 -0800

    Revert "[backend] google auth"
    This reverts commit 5b27e51d3607b82ac906255e972630c74f52da26.

--- backend/app/controllers/application_controller.rb
+++ backend/app/controllers/application_controller.rb
@@ -1,10 +1,2 @@
 class ApplicationController < ActionController::API
-  include SessionsHelper
-  before_action :require_login
-
-  def require_login
-    return if current_user
-
-    render json: { error: "Authenticate Required" }, status: :unauthorized
-  end
 end
--- backend/config/initializers/omniauth.rb
+++
@@ -1,7 +0,0 @@
-Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2,
-           Rails.application.credentials.google[:client_id],
-           Rails.application.credentials.google[:client_secret]
-
-  OmniAuth.config.allowed_request_methods = %i[post get]
-end
