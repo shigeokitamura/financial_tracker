d4546da278f1b55edd190c572bbb6ebac2448d2a
Shigeo Kitamura <shigeo9462@gmail.com>
2025-02-03 22:45:20 -0800

    renew app

--- backend/app/serializers/transaction_serializer.rb
+++
@@ -1,6 +0,0 @@
-class TransactionSerializer < ActiveModel::TransactionSerializer
-  attributes :id, :amount, :currency, :title, :date, :description, :created_at
-
-  belongs_to :category
-  belongs_to :payment_method
-end
