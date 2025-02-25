163cd728a40707d3e7a1652d47f9c8addf586663
Shigeo Kitamura <shigeo9462@gmail.com>
2025-02-25 15:03:48 -0800

    [backend] create Transaction model

---
+++ backend/db/migrate/20250207220610_create_transactions.rb
@@ -0,0 +1,14 @@
+class CreateTransactions < ActiveRecord::Migration[7.2]
+  def change
+    create_table :transactions do |t|
+      t.references :user, null: false, foreign_key: true
+      t.string :name, null: false
+      t.date :date, null:false
+      t.decimal :amount, null: false
+      t.string :currency, null: false
+      t.string :description
+
+      t.timestamps
+    end
+  end
+end
--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,9 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_02_06_222013) do
+ActiveRecord::Schema[7.2].define(version: 2025_02_07_220610) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "transactions", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.string "name", null: false
+    t.date "date", null: false
+    t.decimal "amount", null: false
+    t.string "currency", null: false
+    t.string "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_transactions_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email"
@@ -22,4 +34,6 @@ ActiveRecord::Schema[7.2].define(version: 2025_02_06_222013) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  add_foreign_key "transactions", "users"
 end
