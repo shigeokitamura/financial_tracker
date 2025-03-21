26da2efbf73e0640aa8503c077167816ba417785
Shigeo Kitamura <shigeo9462@gmail.com>
2025-03-21 15:22:35 -0700

    [backend] apply rubocop

--- backend/app/controllers/api/v1/users_controller.rb
+++ backend/app/controllers/api/v1/users_controller.rb
@@ -1,11 +1,11 @@
 class Api::V1::UsersController < ApplicationController
-  skip_before_action :require_login, only: [:index, :show]
+  skip_before_action :require_login, only: [ :index, :show ]
 
   def index; end
   def show; end
   def update; end
   def destroy; end
-  
+
   def current_user
     if @current_user
       render json: { user: @current_user }
--- backend/config/initializers/omniauth.rb
+++ backend/config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET']
-  OmniAuth.config.allowed_request_methods = [:post, :get] # TODO: allow only post
+  provider :google_oauth2, ENV["GOOGLE_CLIENT_ID"], ENV["GOOGLE_CLIENT_SECRET"]
+  OmniAuth.config.allowed_request_methods = [ :post, :get ] # TODO: allow only post
   OmniAuth.config.silence_get_warning = true
 end
