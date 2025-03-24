8c6ff7718e373424d8f7852ceb1b078b18ecac70
Shigeo Kitamura <shigeo9462@gmail.com>
2025-03-24 13:33:31 -0700

    [backend] change cors setting

--- backend/config/initializers/cors.rb
+++ backend/config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "localhost:5173"
+    origins ENV["FRONTEND_URL"]
 
     resource "*",
       headers: :any,
